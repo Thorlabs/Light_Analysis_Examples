3ddb667d639eaa5bd3c7dd104a561cc49cd4dd5f
Jonathan Schubert <jschubert@thorlabs.com>
2024-03-15 12:00:34 +0100

    - add singleChanScope.py - work on dualChanScope.py to unify code between samples - work on pm103Scope.py to unify code between samples

--- Python/Thorlabs PMxxx Power Meters/scpi/scopeMode/singleChanScope.py
+++ Python/Thorlabs PMxxx Power Meters/scpi/scopeMode/singleChanScope.py
@@ -1,13 +1,13 @@
 """
-Example Thorlabs Power Meter 103 Series Channel Scope Mode Example
+Example Thorlabs Power Meter Singlel Channel Scope Mode Example
 Example Date of Creation                            2024-03-15
 Example Date of Last Modification on Github         2024-03-15
 Version of Python                                   3.11.2
 Version of the Thorlabs SDK used                    anyvisa0.3.0
 ==================
-This examples shows how to configure, execute and fetch results of scope measurement on PM103 series 
-using SCPI commands and anyvisa. The code contains scope configuration for software and hardware 
-triggered scope measurements. 
+This examples shows how to configure, execute and fetch results of scope measurement on single channel
+power meters (Except PM103) using SCPI commands and anyvisa. The code contains scope configuration for 
+software and hardware triggered scope measurements.
 """
 
 from anyvisa import AnyVisa
@@ -16,41 +16,28 @@ import time
 import sys
 import matplotlib.pyplot as plt
 
-def fetchBinaryTuplePM103(inst):
+def fetchBinaryTuple(inst):
     """
-    Parses PM103 tuple response
+    Sends SCPI request to device and parses binary tuple response
     
     Parameters
     ----------
     inst : anyvisa device 
         The device anyvisa object used for communication
-    
+
     Returns
     -------
     list
         list of tuples with relative timestamp followed by measurement value
-    """
-    bytes = inst.read_bytes(4096)
-    byteCnt = len(bytes) - 1
-
-    i = 0
+    """    
+    vals = inst.read_bytes(4096)
+    length = struct.unpack('<I',  bytearray(vals[0     : 0 +  4]))[0]
     
-    #Device response length is 0 bytes long
-    if chr(bytes[0]) == '0':
-        return []
-    
-    #Find , in response before parsing binary data
-    for byte in bytes:
-        byte = chr(byte)
-        i+=1
-        if byte == ',':
-            break
-
     res = []
-    #Parse binary tuple data
-    while i < byteCnt:
-        reltime = struct.unpack('<I', bytearray(bytes[i:i+4]))[0]
-        value   = struct.unpack('<f', bytearray(bytes[i+4:i+ 8]))[0]
+    i = 4
+    while i < 8 * length:
+        reltime  = struct.unpack('<I',  bytearray(vals[i     : i +  4]))[0]
+        value   = struct.unpack('<f',  bytearray(vals[i + 4 : i +  8]))[0]
         res.append([reltime, value])
         i += 8
     return res
@@ -67,7 +54,7 @@ def fetchBinaryData(inst):
     Returns
     -------
     list
-        list of all tuples with relative timestamp followed by power measurements
+        list of all tuples with relative timestamp followed by power measurement
 
     Raises
     -------
@@ -76,12 +63,12 @@ def fetchBinaryData(inst):
     """
     data = [] 
     inst.write("FETC:ARR? 0, 100")
-    data.extend(fetchBinaryTuplePM103(inst))
+    data.extend(fetchBinaryTuple(inst))
 
     if len(data) > 0:
         for x in range (100, 10000, 100):
             inst.write(f"FETC:ARR? {x}, 100")
-            data.extend(fetchBinaryTuplePM103(inst))
+            data.extend(fetchBinaryTuple(inst))
     else:
         raise ValueError("No data to fetch")
     return data
@@ -117,7 +104,7 @@ def plotData(data, hPos_t_us=None, triggerLevel=None, xLimit=10000):
     if triggerLevel is not None:
         plt.plot([0,xLimit], [triggerLevel, triggerLevel], 'g--') #trigger threshold
 
-    plt.title("PM103 Scope Measure Results")
+    plt.title("Single Channel Scope Measure Results")
     plt.ylabel('Power [W]')
     plt.xlabel('Time [us]')
     plt.xlim([0, xLimit])
@@ -149,7 +136,7 @@ def waitForTrigger(inst, timeout=50):
             break; 
         if timeoutCnt > timeout:
             inst.write("ABOR") #Abort measureument
-            TimeoutError("Waiting for trigger timed out")
+            raise TimeoutError("Waiting for trigger timed out")
 
 def normalizeScopeSampleTime(data):
     """
@@ -158,7 +145,7 @@ def normalizeScopeSampleTime(data):
     Parameters
     ----------
     data : two dimensional array 
-        entire scope buffer with triplets out of scope buffer
+        entire scope buffer with tuples out of scope buffer
     """
     startTime = data[0][0]
     for sample in data:       
@@ -167,7 +154,7 @@ def normalizeScopeSampleTime(data):
         else:
             sample[0] = 0xffffffff - startTime + sample[0]
 
-def pmSoftwareScopeMode(inst, powerRange_W = 0.1, avg=1, xLim=None):
+def singleChannelSoftwareScope(inst, powerRange_W = 0.1, avg=1, xLim=None):
     """
         Configures, executes single channel software triggered scope mode and visualize result in graph.
         
@@ -182,7 +169,7 @@ def pmSoftwareScopeMode(inst, powerRange_W = 0.1, avg=1, xLim=None):
         xLim : uint
             Limit range of graph X axis. Use None to show all data.
     """
-    print(">>PM103 Software scope mode example.")
+    print(">>singleChannelSoftware scope mode example.")
     print(inst.query('SYST:ERR?').strip())
 
     inst.write("ABOR") #Abort any previous measureument
@@ -194,6 +181,7 @@ def pmSoftwareScopeMode(inst, powerRange_W = 0.1, avg=1, xLim=None):
     print(inst.query('SYST:ERR?').strip())         
 
     print(f">>Configure SW triggered scope mode. Use avg: {avg}")
+    inst.write("CONF:ARR:CHA")
     inst.write(f"CONF:ARR {avg}") 
     print(inst.query('SYST:ERR?').strip())
 
@@ -209,7 +197,7 @@ def pmSoftwareScopeMode(inst, powerRange_W = 0.1, avg=1, xLim=None):
 
     print(">>Calculate delta t between scope samples")
     normalizeScopeSampleTime(data)
-    
+
     #use time of last sample as xLimit
     if xLim is None:
         xLim = data[-1][0]
@@ -220,9 +208,9 @@ def pmSoftwareScopeMode(inst, powerRange_W = 0.1, avg=1, xLim=None):
     plotData(data, None, None, xLim)
 
 
-def pmHardwareScopeMode(inst, powerRange_W = 0.1, peakThresPerc = 40, trigSrc=1, avg=1, hPos = 0, xLim=None):
+def singleChannelHardwareScope(inst, powerRange_W = 0.1, peakThresPerc = 40, trigSrc=1, avg=1, hPos = 0, xLim=None):
     """
-        Configures, executes  single channel hardware triggered scope mode and visualize result in graph.
+        Configures, executes hardware triggered scope mode and visualize result in graph.
         
         Parameters
         ----------
@@ -233,16 +221,15 @@ def pmHardwareScopeMode(inst, powerRange_W = 0.1, peakThresPerc = 40, trigSrc=1,
         peakThresPerc : float
             Trigger signal threshold in percent of measurement range. Only relevant when trigSrc is 1 or 2. 
         trigSrc : uint
-            hardware trigger source. 1 for channel 1. 2 for DIO1
+            hardware trigger source. For closer details read SCPI command CONF:ARR:HWT dokumentation of device.
         avg : uint
             Averaging of scope. Unit is samples. 1 results in 100 kSPS. 2 results in 50 kSPS.
         hPos : uint
             Horizontal position of trigger. Unit is samples.
         xLim : uint
             Limit range of graph X axis. Use None to show all data.
-
     """
-    print(">>PM103 Hardware scope mode example.")
+    print(">>singleChannelHardware scope mode example.")
     print(inst.query('SYST:ERR?').strip())
 
     inst.write("ABOR") #Abort any previous measureument
@@ -252,19 +239,12 @@ def pmHardwareScopeMode(inst, powerRange_W = 0.1, peakThresPerc = 40, trigSrc=1,
     inst.write('SENS:FREQ:MODE CW')                # Select CW mode. In Peak mode scope is not available
     inst.write('INP:FILT 0')                       # Disable bandwidth limitation
     print(inst.query('SYST:ERR?').strip())
-
-    #Select internal or external trigger
-    if trigSrc == 1: #Set threshold for internal trigger
-        inst.write(f'SENS:PEAK{peakThresPerc}')    # Set threshold for peak detection in percent.
-        inst.write('SOUR:DIG:PIN1:FUNC OALT')      # Configure DIO1 to output pulse on trigger condition
-    else: #Configure IO pin for external trigger
-        inst.write('SOUR:DIG:PIN1:FUNC IALT')      # Select DIO1 as trigger input
-    print(inst.query('SYST:ERR?').strip())
-
-    print(f"Range: {powerRange_W}, Thresh:{peakThresPerc}")
     
+    print(f"Range: {powerRange_W}, Thresh:{peakThresPerc}")
+
     print(f">>Configure HW scope mode. Use avg: {avg}")
-    inst.write(f"CONF:ARR:HWT {avg}, {hPos}") 
+    inst.write("CONF:ARR:CHA")
+    inst.write(f"CONF{trigSrc}:ARR:HWT {avg}, {hPos}") 
     print(inst.query('SYST:ERR?').strip())
 
     print(">>Start measurement")
@@ -305,14 +285,13 @@ def main():
     if not devicesList:
         print("Did not find a single instrument for scope mode")
         sys.exit(1)
-
+    
     #Open device 0 out of find list for communication
     with devicesList[0] as pm:
-        print(pm, pm.lib())
-        #pmSoftwareScopeMode(inst, powerRange_W = 0.1, avg=1, xLim=None)
-        pmSoftwareScopeMode(pm, 0.01)
-        #pmHardwareScopeMode(inst, powerRange_W = 0.1, peakThresPerc = 40, trigSrc=1, avg=1, hPos = 0, xLim=None):
-        #pmHardwareScopeMode(pm, 0.004, 40, 2, 1, 100, 20000)
+        #singleChannelSoftwareScope(inst, powerRange_W = 0.1, avg=1, xLim=None)
+        #singleChannelSoftwareScope(pm, 0.01)
+        #singleChannelHardwareScope(inst, powerRange_W = 0.1, peakThresPerc = 40, trigSrc=1, avg=1, hPos = 0, xLim=None):
+        singleChannelHardwareScope(pm, 0.000150, 20, 1, 1, 50, 6000)
 
 
 if __name__ == '__main__':
