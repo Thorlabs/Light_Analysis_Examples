55fd39b6d5c70d51290ca94f2862961bed375253
Jonathan Schubert <jschubert@thorlabs.com>
2024-04-05 13:19:32 +0200

    - swap file content

--- Python/Thorlabs PMxxx Power Meters/scpi/PMxxx_SCPI_SearchAnyvisa.py
+++ Python/Thorlabs PMxxx Power Meters/scpi/PMxxx_SCPI_SearchAnyvisa.py
@@ -1,14 +1,16 @@
-# Open a known device by resource string
+# Start a device search and open first device found
 from anyvisa import AnyVisa
+devices = []
+#run device search with wildcard
+devices = AnyVisa.FindResources("?*")
 
-#USB example:      "TCPIP0::10.10.4.77::PM5020_07::INSTR"
-#Serial example    "TCPIP0::10.10.4.77::PM5020_07::INSTR"
-#Ethernet example  "TCPIP0::10.10.4.77::PM5020_07::INSTR"
-#BLE example       "TCPIP0::10.10.4.77::PM5020_07::INSTR"
+#Found at least one device?
+if devices:
+    #Use with pattern to ensure all resources are released finally
+    with devices[0] as d:
+        #print device resource string and used library (pyvisa or tlvisa)
+        print(d, d.lib())
+        #print device identification
+        print(d.auto_query("*IDN?"))
 
-#Use with pattern to ensure all resources are released finally
-with AnyVisa.TL_Open("TCPIP0::10.10.4.77::PM5020_07::INSTR") as d:
-    #print device resource string and used library (pyvisa or tlvisa)
-    print(d,d.lib())
-    #print device identification
-    print(d.auto_query("*IDN?"))
+
