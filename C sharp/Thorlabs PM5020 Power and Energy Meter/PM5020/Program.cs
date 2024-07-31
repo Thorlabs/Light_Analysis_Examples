8ba28a125804c86f922f144bed4f7f0747491945
Zoe Zhang <120383504+ZiyingZhang313@users.noreply.github.com>
2024-07-31 17:21:53 +0800

    Update header

--- C sharp/Thorlabs PM5020 Power and Energy Meter/PM5020/Program.cs
+++ C sharp/Thorlabs PM5020 Power and Energy Meter/PM5020/Program.cs
@@ -1,4 +1,12 @@
-﻿using System;
+//Example Date of Creation(YYYY - MM - DD) 2023 - 05 - 24
+//Example Date of Last Modification on Github 2023 - 05 - 24
+//Version of .NET Framework used for Testing: 4.7.2
+//Version of the Thorlabs Dll: 5.6.4927.658
+//Example Description: This example has a GUI interface, including the initialization, parameter reading, parameter setting and value display. 
+//If two power sensors or two energy sensors are connected to the PM5020 simultaneously, data processing like normalization, 
+//sum and difference functions are also available. This example is also compatible with single-channel meters like PM10X series.
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
