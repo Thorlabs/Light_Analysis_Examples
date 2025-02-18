5fb1bb52f5ecdf8ccd871bbd97a4ef63c717056c
gboedecker <125995107+gboedecker@users.noreply.github.com>
2025-02-18 14:15:41 +0100

    Update CCS using ctypes - Python 3.py
    Status query ensures correct data acquisition for longer integration times

--- Python/Thorlabs CCS Spectrometers/CCS using ctypes - Python 3.py
+++ Python/Thorlabs CCS Spectrometers/CCS using ctypes - Python 3.py
@@ -2,9 +2,9 @@
 """
 Example Title: CCS using ctypes - Python 3.py
 Example Date of Creation(YYYY-MM-DD): 2022-05-11
-Example Date of Last Modification on Github: 2022-05-11
-Version of Python used for Testing and IDE: 3.10.0
-Version of the Thorlabs SDK used: ThorSpectra version 3.25
+Example Date of Last Modification on Github: 2025-02-18
+Version of Python used for Testing and IDE: 3.11.0
+Version of the Thorlabs SDK used: ThorSpectra version 3.30
 ==================
 Example Description: This example shows the basic operation of a CCS spectrometer. The necessary settings are made, a spectrum is recorded and then plotted.
 """
@@ -28,13 +28,16 @@ lib.tlccs_init(b"USB0::0x1313::0x8089::M00245773::RAW", 1, 1, byref(ccs_handle))
 integration_time=c_double(10.0e-3)
 lib.tlccs_setIntegrationTime(ccs_handle, integration_time)
 
+wavelengths=(c_double*3648)()
+lib.tlccs_getWavelengthData(ccs_handle, 0, byref(wavelengths), c_void_p(None), c_void_p(None))
 
 #start scan
 lib.tlccs_startScan(ccs_handle)
 
-wavelengths=(c_double*3648)()
-
-lib.tlccs_getWavelengthData(ccs_handle, 0, byref(wavelengths), c_void_p(None), c_void_p(None))
+#wait for acquisition and data fetching finish
+status=c_int(0)
+while(status.value & 0x0010)==0:
+    lib.tlccs_getDeviceStatus(ccs_handle,byref(status))
 
 #retrieve data
 data_array=(c_double*3648)()
@@ -49,5 +52,3 @@ plt.show()
 
 #close
 lib.tlccs_close (ccs_handle)
-
-
